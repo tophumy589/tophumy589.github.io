f09f16f91edd19925489fabab215e62031c64927
tophumy589 <109907120+tophumy589@users.noreply.github.com>
2022-12-19 18:55:15 +0700

    Add files via upload

---
+++ music/apialbum.js
@@ -0,0 +1,43 @@
+let apiAlbum = [
+//12 19
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=kHJnyZLLLaskdLRynTbGZHTZCQchakcFx",
+
+//16 20
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=LGJntkBHJcbWDWNTntDmZHtkhWxhskuQA",
+
+//25 19
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=ZGJmykZFdLaGdCxtHyvHLmtZgQchsbbFl",
+
+//18 20
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=kHxHtLdkAQpzBmxtnyvnkmTLhpJCaLRml",
+  
+//mt bông hoa đẹp nhất
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=ZmxmyLBNzdzWxusyHTFnLGTkhpJChcVJu",
+
+//mt thêm bn lâu
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=LHJHTLbxhZizWQFTnybHZnyZCQchCaRGR",
+
+//mt đếm ngày xa e
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=ZmcmyLDxWLapNWDTnTvHkmtLXWcgXaiCQ",
+
+//mt ng âm phủ
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=LHJHyLDxQJRmiCWtHybmkHtLhpJhgJZmJ",
+
+//mt đừng lo a đợi mà
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=ZHxHTkdmaRdDJagTHyvHLmykXQxhXJFLV",
+
+//mt ai là ng thw e
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=ZnJHtkFcXaWsEzzTHTvHLnyLgpJChxvgb",
+
+//mt từ em mà ra
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=LmJmtLBdkNpkBgWyHTvmkmTkCWcghxAdB",
+
+//viral
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=kGxnTZSivksxJVSTGtDnZHtLXpcCabpHH",
+
+//some hits
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=LGcmyZzEFmvakmsymyFnkmykCWJCNFpAR",
+
+//mt 1 bước yêu vạn dặm đau
+"https://mp3.zing.vn/xhr/media/get-source?type=album&key=LnJnykbchXRiGdByHtbHLHyLXpJChJdzn",
+]
